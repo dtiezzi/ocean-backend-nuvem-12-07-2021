115bb64e906295788f520fc8514a74c2fb6758bf
dtiezzi <dtiezzi@usp.br>
2021-07-12 10:52:31 -0300

    init 1

--- index.js
+++ index.js
@@ -2,28 +2,28 @@ const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
 (async () => {
-    const url = "mongodb://localhost:27017";
-    const dbName = "ocean_bancodados_09_07_2021";
+  const url = "mongodb://localhost:27017";
+  const dbName = "ocean_bancodados_09_07_2021";
 
-    console.info("Conectando ao banco de dados...");
+  console.info("Conectando ao banco de dados...");
 
-    const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+  // const client = await MongoClient.connect(url, { useUnifiedTopology: true });
 
-    console.info("MongoDB conectado com sucesso!");
+  console.info("MongoDB conectado com sucesso!");
 
-    const db = client.db(dbName);
+  // const db = client.db(dbName);
 
-    const app = express();
+  const app = express();
 
-    // Informo ao Express que todo corpo
-    // de requisição será estruturado em JSON
-    app.use(express.json());
+  // Informo ao Express que todo corpo
+  // de requisição será estruturado em JSON
+  app.use(express.json());
 
-    app.get("/hello", function (req, res) {
-        res.send("Hello World");
-    });
+  app.get("/hello", function (req, res) {
+    res.send("Hello World");
+  });
 
-    /*
+  /*
     Lista de Endpoints CRUD
     Create, Read (Single & All), Update, Delete
     Criar, Ler (Individual & Tudo), Atualizar, Remover
@@ -38,64 +38,64 @@ const { MongoClient, ObjectId } = require("mongodb");
     [DELETE] -> Delete
     */
 
-    // Escolha um tema: Filmes, Séries, Jogos, etc
+  // Escolha um tema: Filmes, Séries, Jogos, etc
 
-    const lista = ["Senhor dos Anéis", "Harry Potter"];
-    //              0                   1
+  const lista = ["Senhor dos Anéis", "Harry Potter"];
+  //              0                   1
 
-    const filmes = db.collection("filmes");
+  const filmes = db.collection("filmes");
 
-    // [GET] - Read All
-    app.get("/filmes", async (req, res) => {
-        const listaFilmes = await filmes.find().toArray();
+  // [GET] - Read All
+  app.get("/filmes", async (req, res) => {
+    const listaFilmes = await filmes.find().toArray();
 
-        res.send(listaFilmes);
-    });
+    res.send(listaFilmes);
+  });
 
-    // [GET] - Read Single (ou Read by ID/Index)
-    app.get("/filmes/:id", async (req, res) => {
-        const id = req.params.id;
+  // [GET] - Read Single (ou Read by ID/Index)
+  app.get("/filmes/:id", async (req, res) => {
+    const id = req.params.id;
 
-        const item = await filmes.findOne({ _id: ObjectId(id) });
+    const item = await filmes.findOne({ _id: ObjectId(id) });
 
-        res.send(item);
-    });
+    res.send(item);
+  });
 
-    // [POST] - Create
-    app.post("/filmes", async (req, res) => {
-        const item = req.body;
+  // [POST] - Create
+  app.post("/filmes", async (req, res) => {
+    const item = req.body;
 
-        await filmes.insertOne(item);
+    await filmes.insertOne(item);
 
-        res.send(item);
-    });
+    res.send(item);
+  });
 
-    // [PUT] - Update
-    app.put("/filmes/:id", async (req, res) => {
-        const id = req.params.id;
+  // [PUT] - Update
+  app.put("/filmes/:id", async (req, res) => {
+    const id = req.params.id;
 
-        const item = req.body;
+    const item = req.body;
 
-        await filmes.updateOne({ _id: ObjectId(id) }, { $set: item });
+    await filmes.updateOne({ _id: ObjectId(id) }, { $set: item });
 
-        res.send(item);
-    });
+    res.send(item);
+  });
 
-    // [DELETE] - Delete
-    app.delete("/filmes/:id", async (req, res) => {
-        const id = req.params.id;
+  // [DELETE] - Delete
+  app.delete("/filmes/:id", async (req, res) => {
+    const id = req.params.id;
 
-        await filmes.deleteOne({ _id: ObjectId(id) });
+    await filmes.deleteOne({ _id: ObjectId(id) });
 
-        res.send("Item removido com sucesso.");
-    });
+    res.send("Item removido com sucesso.");
+  });
 
-    app.listen(process.env.PORT || 3000);
+  app.listen(process.env.PORT || 3000);
 
-    // Resumo dos endpoints:
-    // [POST] - /filmes -> Adicionar um elemento
-    // [GET] - /filmes/:id -> Ler um único elemento
-    // [GET] - /filmes -> Let todos os elementos
-    // [PUT] - /filmes/:id -> Alterar um único elemento
-    // [DELETE] - /filmes/:id -> Apagar um único elemento
+  // Resumo dos endpoints:
+  // [POST] - /filmes -> Adicionar um elemento
+  // [GET] - /filmes/:id -> Ler um único elemento
+  // [GET] - /filmes -> Let todos os elementos
+  // [PUT] - /filmes/:id -> Alterar um único elemento
+  // [DELETE] - /filmes/:id -> Apagar um único elemento
 })();
